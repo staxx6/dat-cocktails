ec2e972e3b81c9c32d652602ee8fb0f58954312d
staxx6 <staxx@hotmail.de>
2023-07-03 00:37:55 +0200

    Running server

---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response } from 'express';
+
+const app = express();
+const port = 8000;
+
+app.get('/', (req: Request, res: Response): void => {
+  res.send('Express + TypeScript Server');
+  console.log("Hello World!");
+});
+
+app.listen(port, () => {
+	console.log(`⚡️ [server]: Server is running at http://localhost:${port}`);
+})
