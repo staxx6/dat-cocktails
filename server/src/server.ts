07e122ed76932527fcb24d54ae3cdfd3a81a3d76
staxx6 <staxx@hotmail.de>
2023-07-04 23:41:55 +0200

    connect to mongodb

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,38 @@
-import express, { Application, Request, Response } from 'express';
+import express, {Application, Request, Response} from 'express';
+import {MongoClient} from "mongodb";
+// import {MongoClient, MongoClientOptions} from 'mongodb';
 
 const app = express();
 const port = 8000;
 
-app.get('/', (req: Request, res: Response): void => {
-  res.send('Express + TypeScript Server');
-  console.log("Hello World!");
+// Mongodb
+const mongoUrl = 'mongodb://localhost:27017/';
+const mongoClient = new MongoClient(mongoUrl);
+
+try {
+    await mongoClient.connect();
+} catch (e) {
+    console.error(e);
+}
+
+const mongoDbName = 'dat-cocktails';
+const database = mongoClient.db(mongoDbName);
+const ingredientsCollectionName = 'ingredients';
+const ingredientsCollection = database.collection(ingredientsCollectionName);
+
+// Test get all ingredients
+app.get('/', async (req: Request, res: Response) => {
+    const query = {};
+    const result = await ingredientsCollection.find(query);
+    res.contentType('application/json');
+    const resOutArr = [];
+    for await (const doc of result) {
+        resOutArr.push(doc);
+    }
+    res.send(resOutArr);
+    // console.log(resOutArr);
 });
 
 app.listen(port, () => {
-	console.log(`⚡️ [server]: Server is running at http://localhost:${port}/`);
+    console.log(`⚡️ [server]: Server is running at http://localhost:${port}/`);
 })
